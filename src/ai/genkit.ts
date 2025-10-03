728a0ff85cdc7a6a994591fa4ad46b6ce4f28297
codewithAdarsh1 <adarsh44532@gmail.com>
2025-10-03 20:22:46 +0545

    Updated genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,14 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {groq} from 'genkitx-groq';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GEMINI_API_KEY
+    }),
+    groq({
+      apiKey: process.env.GROQ_API_KEY
     })
   ],
 });
