feeba1d3597bdda0da61f47a4403d82c387e7ed2
Adarsh Chaudhary <adarsh44532@gmail.com>
2025-10-03 08:41:01 +0000

    Try fixing this error: `Console Error: Error: Failed to get data from se

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI({
-    apiVersion: 'v1'
+    apiVersion: 'v1beta'
   })],
   model: 'googleai/gemini-1.5-flash',
 });
